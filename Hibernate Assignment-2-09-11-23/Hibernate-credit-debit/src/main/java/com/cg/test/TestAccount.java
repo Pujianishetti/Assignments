ccf6bfa5a4eb6ea12c2b2f35e436b7cc72420674
Pujianishetti <149366879+Pujianishetti@users.noreply.github.com>
2023-11-16 21:39:39 +0530

    Add files via upload

---
+++ Hibernate Assignment-2-09-11-23/Hibernate-credit-debit/src/main/java/com/cg/test/TestAccount.java
@@ -0,0 +1,21 @@
+package com.cg.test;
+
+import com.cg.util.CrudOperationsAcc;
+import com.cg.util.JPAUtil;
+
+import jakarta.persistence.EntityManager;
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		EntityManager em=JPAUtil.getEntityManagerFactory().createEntityManager();
+		CrudOperationsAcc b=new CrudOperationsAcc();
+		b.insertEntity();
+//		b.findEntity();
+//		b.updateEntity();
+//		b.removeEntity();
+	}
+	
+	
+
+}
